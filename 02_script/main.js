625e0b441d5fc8ce7c9141eeda8b56f46caf348a
Hyggesen <69785485+Hyggesen@users.noreply.github.com>
2020-09-20 20:53:34 +0200

    change

--- 02_script/main.js
+++ 02_script/main.js
@@ -2,7 +2,7 @@ let myImage = document.querySelector("img");
 
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute("src");
-  if (mySrc === "03_images/firefox-icon.png") {
+  if (mySrc === "03_images/ps4.jpg") {
     myImage.setAttribute("src", "03_images/fallguys.png");
   } else {
     myImage.setAttribute("src", "03_images/ps4.jpg");
